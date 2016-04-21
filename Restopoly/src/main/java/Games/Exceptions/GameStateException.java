14ab1b75a7520649facff0b36813090f680d1ad6
mcmyffin <dimitri.meier@haw-haburg.de>
2016-04-21 12:29:30 +0200

    neue Exceptions hinzugefuegt

---
+++ Restopoly/src/main/java/Games/Exceptions/GameStateException.java
@@ -0,0 +1,14 @@
+package Games.Exceptions;
+
+/**
+ * Created by dima on 12.04.16.
+ */
+public class GameStateException extends Exception {
+
+    public GameStateException() {
+    }
+
+    public GameStateException(String message) {
+        super(message);
+    }
+}
---
+++ Restopoly/src/main/java/Games/Exceptions/PlayerSequenceWrongException.java
@@ -0,0 +1,14 @@
+package Games.Exceptions;
+
+/**
+ * Created by dima on 12.04.16.
+ */
+public class PlayerSequenceWrongException extends Exception {
+
+    public PlayerSequenceWrongException() {
+    }
+
+    public PlayerSequenceWrongException(String message) {
+        super(message);
+    }
+}
